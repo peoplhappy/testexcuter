059f437c140454abd9d2fb23a252fc660a14e977
peng  chen <39702717+peoplhappy@users.noreply.github.com>
2019-02-02 10:05:59 +0800

    执行测试微服务
    执行测试微服务

---
+++ src/main/java/org/jtest/app/testexcuter/TestexcuterApplication.java
@@ -0,0 +1,20 @@
+package org.jtest.app.testexcuter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * testcase与testsuites的执行模块
+ * @author pwx322979
+ *
+ */
+@SpringBootApplication
+public class TestexcuterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestexcuterApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/org/jtest/app/testexcuter/TestexcuterApplicationTests.java
@@ -0,0 +1,17 @@
+package org.jtest.app.testexcuter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestexcuterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
